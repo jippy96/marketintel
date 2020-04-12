07a292a1b6fb6d55d76c8dc540f2b220d39a23ae
jippy96 <33454256+jippy96@users.noreply.github.com>
2020-04-12 00:50:20 +0000

    home page building 2

--- marketintel/settings.py
+++ marketintel/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/dev/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/dev/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
--- marketintel/urls.py
+++ marketintel/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # my Own import below
 from . import views
@@ -23,3 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home)
 ]
+
+urlpatterns += staticfiles_urlpatterns()
