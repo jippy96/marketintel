9771e78febf18e183a77ba5c2932f2d043cfa45e
jippy96 <33454256+jippy96@users.noreply.github.com>
2020-04-15 18:27:19 +0000

    insertion des charts

--- static/js/own/chart.js
+++ static/js/own/chart.js
@@ -1,37 +1,136 @@
-var ctx = document.getElementById('myChart');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
         }
-    }
-});
+    });
+}
+
+window.onload = function () {
+
+    //Better to construct options first and then pass it as a parameter
+
+    CanvasJS.addColorSet("greenShades",
+        [//colorSet Array
+
+            "#30E0B9",
+        ]);
+
+    var options = {
+        theme: "light2",
+        colorSet: "greenShades",
+        title: {
+            text: ""
+        },
+        data: [
+            {
+                // Change type to "doughnut", "line", "splineArea", etc.
+                type: "column",
+                dataPoints: [
+                    { label: "apple", y: 10 },
+                    { label: "orange", y: 15 },
+                    { label: "banana", y: 25 },
+                    { label: "mango", y: 30 },
+                    { label: "grape", y: 28 }
+                ]
+            }
+        ]
+    };
+
+    $("#chartContainer1").CanvasJSChart(options);
+
+    // taux de diplomé / cycle
+    var options2 = {
+        animationEnabled: true,
+        title: {
+            text: ""
+        },
+        data: [{
+            type: "doughnut",
+            innerRadius: "60%",
+            showInLegend: false,
+            legendText: "{label}",
+            indexLabel: "{label}: #percent%",
+            dataPoints: [
+                { label: "Department Stores", y: 6492917 },
+                { label: "Discount Stores", y: 7380554 },
+
+            ]
+        }]
+    };
+    $("#chartContainer2").CanvasJSChart(options2);
+
+    // taux de diplomé / cycle
+    var options3 = {
+        animationEnabled: true,
+        title: {
+            text: ""
+        },
+        data: [{
+            type: "doughnut",
+            innerRadius: "60%",
+            showInLegend: false,
+            legendText: "{label}",
+            indexLabel: "{label}: #percent%",
+            dataPoints: [
+                { label: "Department Stores", y: 6492917 },
+                { label: "Discount Stores", y: 7380554 },
+
+            ]
+        }]
+    };
+    $("#chartContainer").CanvasJSChart(options3);
+
+    // taux d'apprenants
+    //Better to construct options first and then pass it as a parameter
+    var options4 = {
+        animationEnabled: true,
+        title: {
+            text: "",
+        },
+        axisY: {
+            tickThickness: 0,
+            lineThickness: 0,
+            valueFormatString: " ",
+            gridThickness: 0
+        },
+        axisX: {
+            tickThickness: 0,
+            lineThickness: 0,
+            labelFontSize: 18,
+            labelFontColor: "Peru"
+        },
+        data: [{
+            indexLabelFontSize: 12,
+            toolTipContent: "<span style=\"color:#62C9C3\">{indexLabel}:</span> <span style=\"color:#CD853F\"><strong>{y}</strong></span>",
+            indexLabelPlacement: "outside",
+            indexLabelFontColor: "black",
+            indexLabelFontWeight: 100,
+            indexLabelFontFamily: "Verdana",
+            color: "#62C9C3",
+            type: "bar",
+            dataPoints: [
+                { y: 21, label: "21%", indexLabel: "Video" },
+                { y: 25, label: "25%", indexLabel: "Dining" },
+                { y: 33, label: "33%", indexLabel: "Entertainment" },
+                { y: 36, label: "36%", indexLabel: "News" },
+                { y: 42, label: "42%", indexLabel: "Music" },
+                { y: 49, label: "49%", indexLabel: "Social Networking" },
+                { y: 50, label: "50%", indexLabel: "Maps/ Search" },
+                { y: 55, label: "55%", indexLabel: "Weather" },
+                { y: 61, label: "61%", indexLabel: "Games" }
+            ]
+        }]
+    };
+
+    $("#chartContainer4").CanvasJSChart(options4);
+}
+
+
