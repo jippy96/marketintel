6c282f9832cc35fd9851ffa972e4579ca4ab8e4a
badou11 <mcabpn@gmail.com>
2020-04-15 16:37:46 +0000

    new file:   ToDo 	new file:   education/__init__.py 	new file:   education/admin.py 	new file:   education/apps.py 	new file:   education/migrations/__init__.py 	new file:   education/models.py 	new file:   education/tests.py 	new file:   education/views.py 	modified:   marketintel/__pycache__/settings.cpython-38.pyc 	modified:   marketintel/__pycache__/urls.cpython-38.pyc 	modified:   marketintel/__pycache__/wsgi.cpython-38.pyc 	modified:   marketintel/settings.py 	modified:   marketintel/urls.py 	modified:   modele/__pycache__/models.cpython-38.pyc

--- marketintel/settings.py
+++ marketintel/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'education.apps.EducationConfig',
     'modele.apps.ModeleConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'marketintel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'marketintel',
+        'USER': 'postgres',
+        'PASSWORD': 'badou',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
