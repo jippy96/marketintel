6f7f653db68264bf0852633b8173d9d272305f91
badou11 <mcabpn@gmail.com>
2020-04-16 17:11:28 +0000

    ajout des vues. Regarde aussi le fichier Todo

--- education/views.py
+++ education/views.py
@@ -1,3 +1,50 @@
+"""
+	Don't forget to create the html page.
+
+"""
+
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+from .models import prescolaire, elementaire, moyen, secondaire
+
+def pie_chart(request, modele):
+    labels = []
+    data = []
+
+    queryset = modele.objects.exclude(region="Sénégal")
+    for elem in queryset:
+        labels.append(elem.region)
+        data.append(elem.total)
+
+    context = {'labels': labels, 'data': data}
+
+    return render(request, 'chart.html', context)
+
+def bar_chart(request, modele):
+    labels = []
+    data = []
+
+    queryset = modele.objects.exclude(region="Sénégal")
+    for elem in queryset:
+        labels.append(elem.region)
+        data.append(elem.total)
+
+    context = {'labels': labels, 'data': data}
+
+    return render(request, 'chart.html', context)
+
+def doughnut_chart(request, modele):
+    labels = []
+    data = []
+
+    queryset = modele.objects.exclude(region="Sénégal")
+    for elem in queryset:
+        labels.append(elem.region)
+        data.append(elem.total)
+
+    context = {'labels': labels, 'data': data}
+
+    return render(request, 'chart.html', context)
