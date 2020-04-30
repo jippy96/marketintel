d7c266ed9a5833b260becd4d06c94cc71025a433
jippy96 <33454256+jippy96@users.noreply.github.com>
2020-04-30 17:25:39 +0000

    insertion de la berre de recherche

--- marketintel/views.py
+++ marketintel/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 def home(request):
     return render(
@@ -6,4 +7,27 @@ def home(request):
         'pages/home.html',{
             
         }
-        )
+    )
+
+# méthode pour la recherche 
+def search(request):
+    if request.GET['q']:
+        tableau = {
+            "success": request.GET['q']
+        }
+        return JsonResponse(tableau)
+    return render(request, 'pages/home.html')
+
+
+def recherche(request):
+    
+    item = request.GET['q']
+    
+    # je lance les requetes de select avec ce joker dans les tables
+    
+    return render(
+        request,
+        'pages/recherche.html',{
+            'item': item,
+        }
+    )
